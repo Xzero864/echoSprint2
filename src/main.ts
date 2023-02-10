c7035e7140c3b39c5c120d24b9deb9697b56d4c7
Jason Silva <littlejsilva@gmail.com>
2023-02-10 16:36:53 -0500

    hello

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,36 @@
 // The window.onload callback is invoked when the window is first loaded by the browser
-
+import * as csv from "./tableCsv.js"
 
 
 window.onload = () => {
     prepareButton();
+    prepareKeyPress();
 
   // If you're adding an event for a button click, do something similar.
   // The event name in that case is "click", not "keypress", and the type of the element
   // should be HTMLButtonElement. The handler function for a "click" takes no arguments.
 };
 
+const prepareKeyPress = () => {
+    const body :HTMLElement | null = document.getElementById("body")
+    if (body != null) {
+        document.addEventListener("keypress",(event) => {
+            if (event.code === "Enter") {
+                handleButton();
+            }
+        })
+    }
+}
+const csvMap = new Map();
+csvMap.set("firstCSV",[["1","2","3","4"], ["5","6","7","8","9"],["10","11","12","13","14"]])
+let curCSV:Array<Array<string>> | null = null;
 let mode = "brief";
+
 
 const textBox: HTMLElement | null = document.getElementById("replText");
 const historyText: HTMLElement | null = document.getElementById("history");
 
-const handleButton = (e: MouseEvent) => {
+const handleButton = () => {
   if (
     textBox != null &&
     textBox instanceof HTMLInputElement &&
@@ -25,21 +40,36 @@ const handleButton = (e: MouseEvent) => {
       const args = textBox.value.split(" ");
       switch (args[0]) {
         case "mode":
-          mode = args[1];
+          mode = (mode === "brief") ? "verbose" : "brief";
           console.log(mode);
+          const modeButton:HTMLElement| null = document.getElementById("mode")
+            if (modeButton != null) {
+                modeButton.textContent = mode;
+            }
           break;
         case "load_file":
+            if (args[1] in csvMap) {
+                curCSV = csvMap.get(args[1]);
+                logText(true,"CSV loaded Successfully")
+
+            } else {
+                logText(true,"CSV failed to load, try again")
+            }
           break;
         case "view":
+          if (curCSV != null) {
+              logText(true,"See table below")
+              csvProcess(curCSV);
+          } else {
+              logText(true,"CSV wasn't loaded")
+          }
           break;
         case "search":
           break;
         default:
       }
       if (mode === "verbose") {
-        const newPara: HTMLParagraphElement = document.createElement("p");
-        newPara.textContent = `Command: ${textBox.value}`;
-        historyText.appendChild(newPara);
+        logText(false,textBox.value);
       }
 
       textBox.value = "";
@@ -63,6 +93,23 @@ const prepareButton = () => {
 
 
     console.log("Button was prepared");
+}
+
+const csvProcess = (inputCSV:Array<Array<string>>) => {
+    if (historyText === null) return;
+    historyText.appendChild(csv.tableCsv(inputCSV,false));
+
+}
+
+const logText = (outPut:boolean, text:string) => {
+    const newPara: HTMLParagraphElement = document.createElement("p");
+    if (outPut) {
+        newPara.textContent = `Output: ${text}`;
+    } else {
+        newPara.textContent = `Command: ${text}`;
+    }
+    if (historyText == null) return;
+    historyText.appendChild(newPara);
 }
 
 // We'll use a global state reference for now
