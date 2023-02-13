85490f27c5886017a6a6aa7a14b13485a776f678
Maia Mongado <maiamongado@maias-mbp.myfiosgateway.com>
2023-02-12 22:02:24 -0500

    added mockedJson

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 // The window.onload callback is invoked when the window is first loaded by the browser
-import * as csv from "./tableCsv.js"
-
+import * as csv from "./tableCsv.js";
+import { firstCSV, petsCSV } from "./mockedJson.js";
 
 window.onload = () => {
-    prepareButton();
-    prepareKeyPress();
+  prepareButton();
+  prepareKeyPress();
 
   // If you're adding an event for a button click, do something similar.
   // The event name in that case is "click", not "keypress", and the type of the element
@@ -12,20 +12,20 @@ window.onload = () => {
 };
 
 const prepareKeyPress = () => {
-    const body :HTMLElement | null = document.getElementById("body")
-    if (body != null) {
-        document.addEventListener("keypress",(event) => {
-            if (event.code === "Enter") {
-                handleButton();
-            }
-        })
-    }
-}
+  const body: HTMLElement | null = document.getElementById("body");
+  if (body != null) {
+    document.addEventListener("keypress", (event) => {
+      if (event.code === "Enter") {
+        handleButton();
+      }
+    });
+  }
+};
 const csvMap = new Map();
-csvMap.set("firstCSV",[["1","2","3","4"], ["5","6","7","8","9"],["10","11","12","13","14"]])
-let curCSV:Array<Array<string>> | null = null;
+csvMap.set("firstCSV", firstCSV);
+csvMap.set("petsCSV", petsCSV);
+let curCSV: Array<Array<string>> | null = null;
 let mode = "brief";
-
 
 const textBox: HTMLElement | null = document.getElementById("replText");
 const historyText: HTMLElement | null = document.getElementById("history");
@@ -40,28 +40,28 @@ const handleButton = () => {
       const args = textBox.value.split(" ");
       switch (args[0]) {
         case "mode":
-          mode = (mode === "brief") ? "verbose" : "brief";
+          mode = mode === "brief" ? "verbose" : "brief";
           console.log(mode);
-          const modeButton:HTMLElement| null = document.getElementById("mode")
-            if (modeButton != null) {
-                modeButton.textContent = mode;
-            }
+          const modeButton: HTMLElement | null =
+            document.getElementById("mode");
+          if (modeButton != null) {
+            modeButton.textContent = mode;
+          }
           break;
         case "load_file":
-            if (args[1] in csvMap) {
-                curCSV = csvMap.get(args[1]);
-                logText(true,"CSV loaded Successfully")
-
-            } else {
-                logText(true,"CSV failed to load, try again")
-            }
+          if (args[1] in csvMap) {
+            curCSV = csvMap.get(args[1]);
+            logText(true, "CSV loaded Successfully");
+          } else {
+            logText(true, "CSV failed to load, try again");
+          }
           break;
         case "view":
           if (curCSV != null) {
-              logText(true,"See table below")
-              csvProcess(curCSV);
+            logText(true, "See table below");
+            csvProcess(curCSV);
           } else {
-              logText(true,"CSV wasn't loaded")
+            logText(true, "CSV wasn't loaded");
           }
           break;
         case "search":
@@ -69,7 +69,7 @@ const handleButton = () => {
         default:
       }
       if (mode === "verbose") {
-        logText(false,textBox.value);
+        logText(false, textBox.value);
       }
 
       textBox.value = "";
@@ -81,36 +81,34 @@ const handleButton = () => {
   }
 };
 const prepareButton = () => {
-    console.log("Button was prepared");
-    const maybeButton: HTMLElement | null = document.getElementById('submitButton');
-    if (maybeButton === null) {
-        console.log("Button could not be found");
-    } else if (maybeButton instanceof HTMLButtonElement) {
-        console.log("event listener added");
-        maybeButton.addEventListener("click", handleButton)
+  console.log("Button was prepared");
+  const maybeButton: HTMLElement | null =
+    document.getElementById("submitButton");
+  if (maybeButton === null) {
+    console.log("Button could not be found");
+  } else if (maybeButton instanceof HTMLButtonElement) {
+    console.log("event listener added");
+    maybeButton.addEventListener("click", handleButton);
+  }
 
-    }
+  console.log("Button was prepared");
+};
 
+const csvProcess = (inputCSV: Array<Array<string>>) => {
+  if (historyText === null) return;
+  historyText.appendChild(csv.tableCsv(inputCSV, false));
+};
 
-    console.log("Button was prepared");
-}
-
-const csvProcess = (inputCSV:Array<Array<string>>) => {
-    if (historyText === null) return;
-    historyText.appendChild(csv.tableCsv(inputCSV,false));
-
-}
-
-const logText = (outPut:boolean, text:string) => {
-    const newPara: HTMLParagraphElement = document.createElement("p");
-    if (outPut) {
-        newPara.textContent = `Output: ${text}`;
-    } else {
-        newPara.textContent = `Command: ${text}`;
-    }
-    if (historyText == null) return;
-    historyText.appendChild(newPara);
-}
+const logText = (outPut: boolean, text: string) => {
+  const newPara: HTMLParagraphElement = document.createElement("p");
+  if (outPut) {
+    newPara.textContent = `Output: ${text}`;
+  } else {
+    newPara.textContent = `Command: ${text}`;
+  }
+  if (historyText == null) return;
+  historyText.appendChild(newPara);
+};
 
 // We'll use a global state reference for now
 
---
+++ src/mockedJson.ts
@@ -0,0 +1,14 @@
+const firstCSV = [
+  ["1", "2", "3", "4"],
+  ["5", "6", "7", "8", "9"],
+  ["10", "11", "12", "13", "14"],
+];
+
+const petsCSV = [
+  ["owner_name", "pet_one", "pet_two"],
+  ["henry", "fido", "spot"],
+  ["camilla", "waggy", "fido"],
+  ["richard", "fido", "doggie"],
+];
+
+export { firstCSV, petsCSV };
